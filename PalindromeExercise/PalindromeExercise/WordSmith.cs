f994f6c94fb46ff551d24c1d1d83341a6b1c0c2d
Alivia-m <aliviagayle@icloud.com>
2021-07-05 16:45:30 -0500

    completed

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+        }
+
+        public bool IsAPalindrome(string word)
+        {
+            var reverse = "";
+
+            for(int i = word.Length; i < word.Length; i--)
+            {
+                reverse += word[i];
+            }
+            if (reverse == word)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,23 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("tenet", true)]
+        [InlineData("kayak", true)]
+        [InlineData("street", false)]
+        [InlineData("beach", false)]
+        [InlineData("racecar", true)]
+        [InlineData("mars", false)]
+        public void Test1(string word, bool expected)
         {
+            //Assert
+            var test = new WordSmith();
 
+            //Act
+            var actual = test.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
